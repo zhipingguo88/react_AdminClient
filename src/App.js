e4d0945b733743e716833eb5ff16301a20f7f498
zhipingguo88 <zhipingguo88@gmail.com>
2020-03-27 22:04:36 +0800

    add react-router-dom && create the basic router pages between admin and login

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@
 
 import React,{Component} from 'react'
 import {Button,message} from 'antd'
+import {BrowserRouter,HashRouter,Switch,Route} from 'react-router-dom'
+
+import Login from './pages/login/Login'
+import Admin from './pages/admin/Admin'
+
+
 export default class App extends Component{
     handleClick=()=>{
         message.success('成功啦！')
     }
     render(){
         return(
-            <Button type='primary' onClick={this.handleClick}>学习</Button>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/" component={Admin}/>
+                </Switch>
+            </BrowserRouter>
         )
     }
 }

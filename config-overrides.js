e43a1e983af20385de3fde245b66845f6abad43b
zhipingguo88 <zhipingguo88@gmail.com>
2020-03-27 20:09:36 +0800

    add package less && less-loader, change config of less primary-color from blue to green

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,13 @@
-const {override,fixBabelImport}=require('customize-cra')
+const {override,fixBabelImport,addLessLoader} = require('customize-cra')
 
 module.exports = override(
     fixBabelImport('import',{ //配置上babel-plugin-import
         libraryName:'antd',  //针对的是antd
         libraryDirectory:'es',//源码文件夹中的es文件夹
         style:'css',  //自动打包相关的css
+    }),
+    addLessLoader({
+        javascriptEnabled:true,
+        modifyVars:{'@primary-color':'#1DA57A'},// 定义primary-color的颜色从蓝色变成绿色
     }),
 );

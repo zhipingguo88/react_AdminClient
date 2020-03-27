fdd6db03bd744a18fef22ccc3e098d0aebd1dc06
zhipingguo88 <zhipingguo88@gmail.com>
2020-03-27 20:28:28 +0800

    debug

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,7 @@
-const {override,fixBabelImport,addLessLoader} = require('customize-cra')
+const {override,fixBabelImports,addLessLoader} = require('customize-cra')
 
 module.exports = override(
-    fixBabelImport('import',{ //配置上babel-plugin-import
+    fixBabelImports('import',{ //配置上babel-plugin-import
         libraryName:'antd',  //针对的是antd
         libraryDirectory:'es',//源码文件夹中的es文件夹
         style:'css',  //自动打包相关的css

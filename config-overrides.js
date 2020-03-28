d1ad3a30dfb036c28a79b887fa4f7e00fb7e3374
zhipingguo88 <zhipingguo88@gmail.com>
2020-03-28 10:20:02 +0800

    debug

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,13 @@
-const {override,fixBabelImports,addLessLoader} = require('customize-cra')
+const {override, fixBabelImports, addLessLoader} = require('customize-cra');
 
 module.exports = override(
-    fixBabelImports('import',{ //配置上babel-plugin-import
-        libraryName:'antd',  //针对的是antd
-        libraryDirectory:'es',//源码文件夹中的es文件夹
-        style:'css',  //自动打包相关的css
-    }),
-    addLessLoader({
-        javascriptEnabled:true,
-        modifyVars:{'@primary-color':'#1DA57A'},// 定义primary-color的颜色从蓝色变成绿色
-    }),
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: {'@primary-color': '#1DA57A'},
+  }),
 );

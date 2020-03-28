6d2e12dd963582d0ada64e0295a7a6c08ab2795a
zhipingguo88 <zhipingguo88@gmail.com>
2020-03-28 16:45:15 +0800

    add get click value in the login component

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -8,6 +8,7 @@ const Item = Form.Item
 
 export default class login extends Component {
   onFinish = values => {
+    console.log(values.username,values.password)
     alert('发送ajax请求')
   }
   render() {
@@ -21,12 +22,22 @@ export default class login extends Component {
         <section className='login-content'>
           <h3>用户登陆</h3>
 
-          <Form onFinish={this.onFinish} className="login-form">
-            <Item>
+          <Form name='user' onFinish={this.onFinish} className="login-form">
+            <Item name='username' rules={[
+              {required:true,message:'用户名是必须的'},
+              {min:4,message:'用户名不能小于4位'},
+              {max:12,message:'用户名不能大于12位'},
+              {pattern:/^[a-zA-Z0-9]+$/,message:'用户名是必须是英文数字或下划线'},
+              ]}>
               <Input prefix={<UserOutlined type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
                      placeholder="用户名"/>
             </Item>
-            <Item>
+            <Item name='password' rules={[
+              {required:true,whitespace:true,message:'用户名是必须的'},
+              {min:4,message:'用户名不能小于4位'},
+              {max:12,message:'用户名不能大于12位'},
+              {pattern:/^[a-zA-Z0-9]+$/,message:'用户名是必须是英文数字或下划线'},
+              ]}>
               <Input prefix={<LockOutlined type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
                      type="password" placeholder="密码"/>
             </Item>
@@ -42,3 +53,17 @@ export default class login extends Component {
     );
   }
 }
+
+
+/* 
+用户名/密码的合法性要求
+  1)  必须输入
+  2） 必须大于等于4位
+  3） 必须小于等于12位
+  4） 必须是英文、数字或者下划线组成
+
+*/
+/* 
+组件：组件类，本质是一个构造函数
+组件对象：组件类的实例，也就是构造函数的实例
+*/
